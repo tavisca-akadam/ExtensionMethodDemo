e890a4f0b5402452ec7bffb4b8f0ca7f35d6e009
tavisca-akadam <ankadam@tavisca.com>
2019-09-11 13:49:17 +0530

    Use Piggybanking for extension method

---
+++ ExtensionMethodDemo/src/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace System
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToXmlDateTime(this DateTime dateTime)
+        {
+            return XmlConvert.ToString(dateTime,XmlDateTimeSerializationMode.Utc);
+        }
+        public static string ToXmlDateTime(this DateTime dateTime, XmlDateTimeSerializationMode mode)
+        {
+            return XmlConvert.ToString(dateTime, mode);
+        }
+    }
+}
---
+++ LagacyExtensionsTests/DateTimeExtensionsFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LagacyExtensionsTests
+{
+    public class DateTimeExtensionsFixture
+    {
+        [Fact]
+        public void XmlDateTime_should_return_dateTime_in_year_month_date_seperated_by_dashes()
+        {
+            var dateTime = new DateTime(2013, 10, 24, 13, 10, 35, 951);
+            Assert.Equal("2013-10-24T13:10:35.951Z", dateTime.ToXmlDateTime());
+        }
+
+        [Fact]
+        public void XmlDateTime_should_return_dateTime_in_year_month_date_seperated_by_dashes_with_specifying_localization_format()
+        {
+            var dateTime = new DateTime(2013, 10, 24, 13, 10, 35, 951);
+            Assert.Equal("2013-10-24T13:10:35.951+05:30", dateTime.ToXmlDateTime(System.Xml.XmlDateTimeSerializationMode.Local));
+        }
+    }
+}
